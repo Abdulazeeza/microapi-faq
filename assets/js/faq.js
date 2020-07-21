b7f1a1a9624ab42a935985f1a6dd0b226faa5b8d
unknown <abdulazeeza180@gmail.com>
2020-07-22 00:19:48 +0100

    first commit

---
+++ assets/js/faq.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	$(document).on("click", ".block__question", function(){
+		
+		if($(this).hasClass("block__question--active")){
+			$(".block__question--active .block__question__header .block__question__header__btn").toggleClass("block__question__header__btn--active");
+			$(this).toggleClass("block__question--active");
+		}else{
+			$(".block__question--active .block__question__header .block__question__header__btn").toggleClass("block__question__header__btn--active");
+			$(".block__question--active").toggleClass("block__question--active");
+			$(this).toggleClass("block__question--active");
+			$(".block__question--active .block__question__header .block__question__header__btn").toggleClass("block__question__header__btn--active");
+		}
+		
+	});
+});
